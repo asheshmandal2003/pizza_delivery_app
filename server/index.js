be4341c20db77d52735e9e7506c6372c05b688ab
Ashesh Mandal <116034358+asheshmandal2003@users.noreply.github.com>
2023-10-20 22:28:42 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ const sessionOptions = {
   },
 };
 
-app.use(cors({ origin: `${process.env.FRONTEND_URL}/*` }));
+app.use(cors({ origin: `${process.env.FRONTEND_URL}` }));
 app.use(session(sessionOptions));
 app.use(express.json());
 app.use(passport.initialize());
